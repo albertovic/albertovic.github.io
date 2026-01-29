b0f314212ea33686d36eb028217d26dca5eeb3d5
albertovic <61287914+albertovic@users.noreply.github.com>
2026-01-29 17:02:09 +0000

    Deploying to gh-pages from @ albertovic/albertovic.github.io@39d1e23eb7c9420ca0accc3770b1e9f7866c3ef8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -57,9 +57,9 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+            },},{id: "projects-semantic-annotation-of-lidar-data",
+          title: 'Semantic Annotation of LiDAR Data',
+          description: "Bridging the gap between raw depth data and semantic understanding using Foundation Models.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
